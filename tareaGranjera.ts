aea6d5b266d42e329b84001fc4e4c29a0d159b6b
Nehemias-Bava <nehemiasebava@gmail.com>
2023-03-16 21:09:31 -0300

    Tarea

---
+++ tareaGranjera.ts
@@ -0,0 +1,25 @@
+//programa1
+console.log("Primer Algoritmo")
+
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("girar 90 grados")
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("avanzar uno")
+console.log("quitar uno")
+console.log("avanzar uno")
+
+//programa2
+console.log("Segundo Algoritmo")
+
