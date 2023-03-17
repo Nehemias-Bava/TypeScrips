f06e7767dae097c81ec61e2d8631b48759b4012d
Nehemias-Bava <nehemiasebava@gmail.com>
2023-03-16 21:09:04 -0300

    Trabajos

--- holaMundo.ts
+++ holaMundo.ts
@@ -1,1 +1,7 @@
-console.log("Hola Mundo");
+var array = [{ Tarea: "Seguir los siguientes pasos" },
+    { Primero: "apagar el pc" },
+    { Segundo: "abrir el gabinete y quitas el cooler del procesador" },
+    { Tercero: "limpiar cuidadosamente el procesador con alcohol isopropilico" },
+    { Cuarto: "paso colocar la pasta termica de preferencia" },
+    { Quinto: "y ultimo paso, volver a colocar el cooler" }];
+console.log(array);
