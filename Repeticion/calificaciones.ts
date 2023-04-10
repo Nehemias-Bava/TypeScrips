c2f18ef8a5c164115fbb27237a5306bc323cbbec
Nehemias-Bava <nehemiasebava@gmail.com>
2023-04-10 15:03:32 -0300

    Trabajos

--- Repeticion/calificaciones.ts
+++ Repeticion/calificaciones.ts
@@ -19,7 +19,7 @@ let notaFinal : number;
 let nota1 : number = 0;
 let nota2 : number = 0;
 let nota3 : number = 0;
-let contador : number = 0;
+
 
     while(nombre !== ""){
         nombre = rls.question("Ingrese el nombre: ");                                                                                       
@@ -29,7 +29,7 @@ let contador : number = 0;
 
 
         if(nota1 <= 0 || nota1 > 10 || nota2 <= 0 || nota2 > 10 || nota3 <= 0 || nota3 > 10){
-            console.log("Error")
+            console.log("Error");
         }else{
             notaFinal = (nota1 * 0.10 + nota2 * 0.50 + nota3 * 0.40);
             console.log("La nota final de", [nombre], " es de:", [notaFinal]);
