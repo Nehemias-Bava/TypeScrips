380945c5194b925d11325db12b839cda34d6ee0c
Nehemias-Bava <nehemiasebava@gmail.com>
2023-04-10 21:16:46 -0300

    Trabajos

--- Repeticion/dados.js
+++ Repeticion/dados.js
@@ -1,3 +1,4 @@
+"use strict";
 /*• Al tirar un dado tenemos 1/6 de probabilidades de sacar 6
 • Si tiramos dos dados tenemos 1/36 probabilidades de sacar doble 6
 • Al aumentar el número de dados la probabilidad de sacar todos 6 es cada
@@ -5,16 +6,10 @@ vez menor.
 
 • Escriba un programa que calcule la probabilidad de sacar todos los dados 6
 siendo que tiramos N dados (dato ingresado por el usuario) */
-
-
-
-import * as rls from "readline-sync"
-
-const dado : number = 1/6;
-let nDados : number = rls.questionInt("Con cuantos dados tiras?: ");
-let probabilidad : number = 0;
-
-
-probabilidad = dado ** nDados;
-
+exports.__esModule = true;
+var rls = require("readline-sync");
+var dado = 1 / 6;
+var nDados = rls.questionInt("Con cuantos dados tiras?: ");
+var probabilidad = 0;
+probabilidad = Math.pow(dado, nDados);
 console.log("La probabilidad de sacar todos los dados 6 es de:", probabilidad);
---
+++ Repeticion/palindromos.ts
@@ -0,0 +1,10 @@
+/* Consigna de metodo:
+Implemente un método llamado esPalindromo que reciba una cadena de texto y evalue si es un palindromo o no. 
+el metodo debe devolver un booleano y debe ser true para los textos que si son palindromos y false para aquellos 
+que no. */
+
+
+
+function esPalindromo (){
+    
+}
