64556501139704a376a9b85f100b2a498c036803
Nehemias-Bava <nehemiasebava@gmail.com>
2023-04-15 18:32:18 -0300

    Trabajos

---
+++ Modularizacion/potencias.ts
@@ -0,0 +1,9 @@
+/* .Realice un programa que devuelva la potencia de un numero.
+.La base y el exponente deben ser ingresados por teclado.
+.Deben admitirce solo exponentes mayores o iguales a cero.
+.Recuerde que el resultado de un numero elevado a 0 es 1.
+.Separe la logica de calcular la potencia utilizando metodos. */
+
+import * as rls from "readline-sync"
+
+
