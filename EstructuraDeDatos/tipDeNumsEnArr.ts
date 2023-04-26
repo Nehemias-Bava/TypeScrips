7810273d3bc9d17fad549d1688a2b13342c6d23d
Nehemias-Bava <nehemiasebava@gmail.com>
2023-04-26 20:32:34 -0300

    TAREA

--- EstructuraDeDatos/tipDeNumsEnArr.ts
+++ EstructuraDeDatos/tipDeNumsEnArr.ts
@@ -32,11 +32,6 @@ for (const num of numeros) {
     ceros++;
     }
 }
-// ${.sort} Es un metodo de comparacion de array's;
-// Devuelve un número negativo si 'a' es menor que 'b', cero si son iguales y un número positivo si 'a' es mayor que 'b';
-numeros.sort((a, b) => a - b);
-
-console.log("V = ", numeros.join(", "));
 console.log(`Números positivos: ${positivos}`);
 console.log(`Números negativos: ${negativos}`);
 console.log(`Números ceros: ${ceros}`);
